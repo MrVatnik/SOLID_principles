6342452c806fc625d91dc56f5a048006ae1f84ae
Valerii Vatnik <zacvars@gmail.com>
2024-01-17 20:16:43 +0200

    Добавьте файлы проекта.

---
+++ SOLID_principles/SRP.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP //Single Responsibility Principle
+{
+    //this class implements SRP. SRP is: "A module should be responsible to one, and only one, actor."
+    //The essence of the principle is that a class/structure/method should perform one single task.
+    //All functionality of the component must be integral and have high cohesion.
+    //This principle is necessary when it is necessary to change the program.
+    //In the case where it is not applied, changes in one object may lead to changes in another, and so on.
+    //Once it is applied, all changes that need to be made are minimized.
+    //For example, here, if you need to add a new report output method, you can easily add it to the printer method.
+    class Report
+    {
+        public string Text { get; set; } = "";
+        public void GoToFirstPage() =>
+            Console.WriteLine("Go to first page");
+
+        public void GoToLastPage() =>
+            Console.WriteLine("Go to last page");
+
+        public void GoToPage(int pageNumber) =>
+            Console.WriteLine($"Go to page {pageNumber}");
+    }
+    //  обязанность - печать отчета
+    class Printer
+    {
+        public void PrintReport(Report report)
+        {
+            Console.WriteLine("Printing report");
+            Console.WriteLine(report.Text);
+        }
+    }
+}
