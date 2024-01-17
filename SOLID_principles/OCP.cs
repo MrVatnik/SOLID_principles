63d29bf6c298c58c2166c58cb950960aa9e2094a
Valerii Vatnik <zacvars@gmail.com>
2024-01-17 20:47:36 +0200

    OCP

---
+++ SOLID_principles/OCP.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP // Open–closed principle
+{
+    //this class implements OCP. OCP is:
+    //"Software entities ... should be open for extension, but closed for modification."
+    //The point of this principle is that the system should be built in such a way that all subsequent changes
+    //to it should be implemented by adding new code, and not changing existing ones.
+    //It helps when there is collective work, and everyone is responsible for their own part. 
+    //when you need to add new functions you shouldn't change some other guy's code, better extend it with yours.
+
+    //for Example: here we have an abstract class for MealBase.
+    //In this class i described a standart 3-part process for cooking a meal: Prepare ingridients (Prepare());
+    //Cook it (Cook());   and make some Final Steps (FinalSteps()).
+    //For making any kind of meal we should make this steps. So, for every different meal we should just implement
+    //this abstract class.
+    class restaurant
+    {
+        public void makeDinner()
+        {
+            MealBase[] menu = new MealBase[] { new PotatoMeal(), new SaladMeal() };
+
+            Cook bob = new Cook("Bob");
+            bob.MakeDinner(menu);
+        }
+
+
+        class Cook
+        {
+            public string Name { get; set; }
+
+            public Cook(string name)
+            {
+                this.Name = name;
+            }
+
+            public void MakeDinner(MealBase[] menu)
+            {
+                foreach (MealBase meal in menu)
+                    meal.Make();
+            }
+        }
+
+        abstract class MealBase
+        {
+            public void Make()
+            {
+                Prepare();
+                Cook();
+                FinalSteps();
+            }
+            protected abstract void Prepare();
+            protected abstract void Cook();
+            protected abstract void FinalSteps();
+        }
+
+        class PotatoMeal : MealBase
+        {
+            protected override void Cook()
+            {
+                Console.WriteLine("Put potato on fire");
+                Console.WriteLine("Boil for 30 min");
+                Console.WriteLine("Drain the remaining water, mash the boiled potatoes");
+            }
+
+            protected override void FinalSteps()
+            {
+                Console.WriteLine("Sprinkle mashed potatoes with spices and herbs");
+                Console.WriteLine("Mashed potatoes are ready");
+            }
+
+            protected override void Prepare()
+            {
+                Console.WriteLine("Peel and wash the potatoes");
+            }
+        }
+
+        class SaladMeal : MealBase
+        {
+            protected override void Cook()
+            {
+                Console.WriteLine("Slice tomatoes and cucumbers");
+                Console.WriteLine("Sprinkle with herbs, salt and spices");
+            }
+
+            protected override void FinalSteps()
+            {
+                Console.WriteLine("Add oil");
+                Console.WriteLine("The salad is ready");
+            }
+
+            protected override void Prepare()
+            {
+                Console.WriteLine("Wash tomatoes and cucumbers");
+            }
+        }
+    }
+}
--- SOLID_principles/SRP.cs
+++ SOLID_principles/SRP.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace SRP //Single Responsibility Principle
@@ -9,10 +11,17 @@ namespace SRP //Single Responsibility Principle
     //this class implements SRP. SRP is: "A module should be responsible to one, and only one, actor."
     //The essence of the principle is that a class/structure/method should perform one single task.
     //All functionality of the component must be integral and have high cohesion.
-    //This principle is necessary when it is necessary to change the program.
+    //This principle is necessary when it is necessary to change the program. This principle.
+    //Also helps to simplify the understanding of the code, by simplifying each of its component elements to the
+    //simplest tasks that it performs.
     //In the case where it is not applied, changes in one object may lead to changes in another, and so on.
-    //Once it is applied, all changes that need to be made are minimized.
-    //For example, here, if you need to add a new report output method, you can easily add it to the Printer class, without changing anything, even other methods in Printer class.
+    //Once it is applied, all changes that need to be made are minimized. This class may change for two reasons:
+    //1. The content of the report itself may change.
+    //2. The report format may change.
+    //It makes sense that both of these reasons are actually two different responsibilities.
+    //The SRP says that in this case the class should be divided into two new classes, each with only one responsibility.
+    //For example, here, if you need to add a new report output method, you can easily add it to the Printer class,
+    //without changing anything, even other methods in Printer class.
     class Report
     {
         public string Text { get; set; } = "";
